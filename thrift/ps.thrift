cfde7b51330b59dc478efb406785ef7f1b27b456
zhouyong <zhouyong03@meituan.com>
2019-10-27 21:47:24 +0800

    independent communication idl

--- thrift/ps.thrift
+++ thrift/ps.thrift
@@ -2,22 +2,23 @@ namespace cpp   openmi.thrift
 namespace py    openmi.thrift
 namespace java  org.openmi.thrift
 
-# Define service for commincation between ps and worker
+# Define service for communication between ps and worker
 struct ModelInfo {
   1: string model_name,
-  2: string group_name,
-  3: i64 model_index,
+  2: i64 model_index,
 }
 
 service Ps
 {
-  # Key op 
-  list<binary> Pull(1:list<string> names, 2:list<binary> reqs, 3:string val_type, 4:i64 log_id),
+  # pull/push communication
+  list<binary> Pull(1:string name, 2:list<binary> reqs, 3:string value_type, 4:string req_id),
 
-  oneway void Push(1:list<string> names, 2:list<binary> reqs, 3:string val_type, 4:i32 epoch=0, 5:i64 log_id),
-  
-  # Model op in ps side. format: PROTO/PBTXT, val_type: WEIGHTS/PARAMS/GRADS
-  list<string> Create(1:string name, 2:list<binary> pb_defs, 3:bool pb_binary=false), 
+  oneway void Push(1:string name, 2:list<binary> reqs, 3:string value_type, 4:i32 epoch=0, 5:string req_id),
+
+  # create a graph by graph_def 
+  string Create(2:binary graph_def, 3:bool pb_binary = false),
+
+  binary ModelDef(1:string name, 2:bool pb_binary=false), 
   
   void Drop(1:string name),
   
@@ -30,8 +31,6 @@ service Ps
   void Load(1:string name, 2:string path),
   
   list<ModelInfo> ListModels(), 
-
-  binary ModelDef(1:string name, 2:bool pb_binary=false), 
 
   void Move(1:string from_name, 2:string to_name, 3:string backup_name),
   
